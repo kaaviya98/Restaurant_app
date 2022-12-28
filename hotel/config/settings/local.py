562a76e08a346ae605b3950646f4c96dd2bee199
kaaviya98 <kaaviya@testpress.in>
2022-12-28 15:55:46 +0530

    Initialize django-project and create restaurant app

---
+++ hotel/config/settings/local.py
@@ -0,0 +1,18 @@
+import environ
+
+
+env = environ.Env()
+environ.Env.read_env()
+
+DEBUG = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": env("DATABASE_ENGINE"),
+        "NAME": env("DATABASE_NAME"),
+        "USER": env("DATABASE_USER"),
+        "PASSWORD": env("DATABASE_PASSWORD"),
+        "HOST": env("HOST"),
+        "PORT": env("PORT"),
+    }
+}
